91a568cf956fa8fb7bd0a7ea921a7786f430c8a8
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-08-24 11:49:50 -0300

    Remove build from repository

--- x86-ubuntu-env/build.sh
+++
@@ -1,2 +0,0 @@
-docker build --platform linux/x86_64 -t ubuntu20:1 .
-docker run -it -d -v ~/Documents/university/compiladores/preproyecto--nolasco-parladorio:/home/usr/compilers -e TERM=xterm-256color --name ubuntu20 ubuntu20:1
