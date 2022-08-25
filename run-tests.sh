88052386a83fe30fb3a9b2a85a61346f69a6ad67
Jere <jeremiasparladorio@gmail.com>
2022-08-25 16:33:35 -0300

    Add relevant exit status to testing script

--- run-tests.sh
+++ run-tests.sh
@@ -6,10 +6,13 @@ sh script.sh
 
 echo "RUNNING TESTS FOR VALID PROGRAMS...\n"
 
+exit_status=0
+
 for test in tests/acceptedPrograms/*.txt; do
     ./a.out $test > /tmp/out
     if [ $? -eq 1 ]
     then
+       exit_status=1
        echo "In test: $test \nSYNTAX ERROR:\n"
        cat /tmp/out
     fi
@@ -21,6 +24,9 @@ for test in tests/invalidPrograms/*.txt; do
     ./a.out $test > /tmp/out
     if [ $? -eq 0 ]
     then
+      exit_status=1
       echo "In test: $test \nFailed to find the syntax error!\n"
     fi
 done
+
+exit $exit_status
