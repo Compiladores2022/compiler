0c2117d94c3d7b2d5b0f645180155f4edee24bb1
Jere <jeremiasparladorio@gmail.com>
2022-08-30 23:55:12 -0300

    Add symbol.c (not working)

--- src/symbols-table/symbol.c
+++ src/symbols-table/symbol.c
@@ -1,14 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "symbol.h"
 #include "flags.h"
 #include "types.h"
 #include "../list/list.h"
 
 void addSymbol(list l, symbol s) {
-    // TODO: Implement
+    add(l, &s);
 }
 
 symbol searchSymbol(list l, char* name) {
-    // TODO: Implement
-    symbol s;
-    return s;
+    node* cur = (l.head)->next;
+    symbol* s = (symbol*)malloc(sizeof(symbol));
+    while (cur != NULL) {
+        printf("%s\n",((symbol*) cur->value)->name);
+        if (!strcmp(name, ((symbol*) cur->value)->name))
+            s = (symbol*) cur->value;
+        cur = cur->next;
+    }
+    return *s;
 }
