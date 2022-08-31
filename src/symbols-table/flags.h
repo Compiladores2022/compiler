b1082f04f9d403ab9a591ced448917c5c7ba5f58
Jeremias Parladorio <jeremiasparladorio@gmail.com>
2022-08-31 11:43:57 -0300

    Delete src/symbols-table directory

--- src/symbols-table/flags.h
+++
@@ -1,9 +0,0 @@
-#ifndef FLAGS_H
-#define FLAGS_H
-
-typedef enum {
-VAR,
-FUNC,
-} s_flag;
-
-#endif
--- src/symbols-table/types.h
+++
@@ -1,9 +0,0 @@
-#ifndef TYPES_H
-#define TYPES_H
-
-typedef enum {
-INT,
-BOOL,
-} s_type;
-
-#endif
