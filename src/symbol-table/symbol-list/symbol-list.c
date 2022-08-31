4c61f4e592b8a5d58cbdd9a6b2ad0c5844cf9cb6
Jere <jeremiasparladorio@gmail.com>
2022-08-31 18:24:10 -0300

    Test changes, improve search_symbol_l

--- src/symbol-table/symbol-list/symbol-list.c
+++ src/symbol-table/symbol-list/symbol-list.c
@@ -12,8 +12,7 @@ symbol_t* search_symbol_l(list_t l, char* name) {
     symbol_t* s = NULL;
     while (cur != NULL) {
         if (!strcmp(name, ((symbol_t*) cur->value)->name)) {
-            s = (symbol_t*) cur->value;
-            break;
+            return (symbol_t*) cur->value;
         }
         cur = cur->next;
     }
--- src/symbol-table/test.c
+++ src/symbol-table/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include "symbol-table.h"
 
 int main() {
