8b82b29a29f5c2c7d9600c44f84fefdf0b060fc4
Jere <jeremiasparladorio@gmail.com>
2022-09-04 13:05:47 -0300

    Polish whitespaces in utils.h macros

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -3,25 +3,30 @@
 
 // MACROS
 
-#define SYMBOL_TABLE() { st = init_symtable(); push_level(st); }
+#define SYMBOL_TABLE() {                        \
+        st = init_symtable();                   \
+        push_level(st);                         \
+    }
 
-#define NO_ERRORS() { printf("No errors were found! \n"); }
+#define NO_ERRORS() {                           \
+        printf("No errors were found! \n");     \
+    }
 
-#define ADD_SYMBOL(symbol_name) { \
-        symbol_t* s = create_symbol(); \
-        s->name = symbol_name; \
-        insert_symbol(st, s); \
-        printf("Identifier: %s was added\n", s->name); \
+#define ADD_SYMBOL(symbol_name) {                                       \
+    symbol_t* s = create_symbol();                                      \
+    s->name = symbol_name;                                              \
+    insert_symbol(st, s);                                               \
+    printf("Identifier: %s was added\n", s->name);                      \
  }
 
-#define SEARCH_SYMBOL(symbol_name) {            \
-        symbol_t* s = search_symbol(st, symbol_name); \
-        if (s == NULL) { \
-            printf("Error - Undeclared identifier: %s\n", symbol_name); \
-            yyerror(); \
-        } else { \
-            printf("Identifier found: %s\n", s->name); \
-        } \
+#define SEARCH_SYMBOL(symbol_name) {                                    \
+    symbol_t* s = search_symbol(st, symbol_name);                       \
+    if (s == NULL) {                                                    \
+        printf("Error - Undeclared identifier: %s\n", symbol_name);     \
+        yyerror();                                                      \
+    } else {                                                            \
+        printf("Identifier found: %s\n", s->name);                      \
+    }                                                                   \
 }
 
 // FUNCTIONS
