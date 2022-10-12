7f652cf42d1ad82ca42111a7342ce4a209151aee
AgusNolasco <nolasco@dc.exa.unrc.edu.ar>
2022-10-12 16:30:42 +0200

    Now traverse_tree considers the new flags

--- src/main/tree/tree-node.c
+++ src/main/tree/tree-node.c
@@ -66,7 +66,7 @@ void traverse_tree(tree_node_t* root, void (*f)(symbol_t*, tree_node_t*)) {
         traverse_tree(root->middle, f);
         (*f)(s, root);
     }
-    if (s->flag == PROG_F) {
+    if (s->flag == PROG_F || s->flag == PROC_F || s->flag == BLOCK_F || s->flag == LINK_F) {
         traverse_tree(root->left, f);
         traverse_tree(root->right, f);
     }
