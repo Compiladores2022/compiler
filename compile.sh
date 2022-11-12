b4cb8dcd9d64aa1d57b28ee47566e7a873eb1bbc
Jere <jeremiasparladorio@gmail.com>
2022-11-12 20:36:52 -0300

    Improve compilation script

--- compile.sh
+++ compile.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 file=$1
-shift
 filename="${file%.*}"
 asm_file="$filename.s"
 
+if [ ! -f ./npc ]; then
+    make npc
+fi
+
 ./npc $file
-gcc $asm_file "$@" -o $filename
+if [ $? -eq 0 ]; then
+    if [ $# -eq 2 ]; then
+        gcc $asm_file $2 -o $filename
+    else
+        gcc $asm_file lib.c -o $filename
+    fi
+   ./$filename
+fi
